aa3f89ac6f84771b6682153f6e57145848dbcd69
andyjefferson <andy@datanucleus.org>
2014-02-26 14:12:30 +0000

    [NUCCORE-1140] Type converters to java.util.Date for java.sql.XXX types

---
+++ src/java/org/datanucleus/store/types/converters/SqlTimestampDateConverter.java
@@ -0,0 +1,41 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.sql.Timestamp;
+
+/**
+ * Class to handle the conversion between java.sql.Timestamp and a java.util.Date form.
+ */
+public class SqlTimestampDateConverter implements TypeConverter<Timestamp, java.util.Date>
+{
+    public Timestamp toMemberType(java.util.Date value)
+    {
+        if (value == null)
+        {
+            return null;
+        }
+
+        return new java.sql.Timestamp(value.getTime());
+    }
+
+    public java.util.Date toDatastoreType(Timestamp time)
+    {
+    	return time;
+    }
+}
