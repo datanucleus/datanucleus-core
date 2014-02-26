666e74679a4ed0d7b488217da20ca73b49801ce9
andyjefferson <andy@datanucleus.org>
2014-02-26 09:17:41 +0000

    [NUCCORE-1139] TypeConverters for BigDecimal-Double and BigInteger-Long

---
+++ src/java/org/datanucleus/store/types/converters/BigDecimalDoubleConverter.java
@@ -0,0 +1,50 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.math.BigDecimal;
+
+import org.datanucleus.exceptions.NucleusDataStoreException;
+
+/**
+ * Class to handle the conversion between java.math.BigDecimal and a Double form.
+ */
+public class BigDecimalDoubleConverter implements TypeConverter<BigDecimal, Double>
+{
+    public BigDecimal toMemberType(Double val)
+    {
+        if (val == null)
+        {
+            return null;
+        }
+
+        try
+        {
+            return BigDecimal.valueOf(val);
+        }
+        catch (NumberFormatException nfe)
+        {
+            throw new NucleusDataStoreException(LOCALISER.msg("016002", val, BigDecimal.class.getName()), nfe);
+        }
+    }
+
+    public Double toDatastoreType(BigDecimal bd)
+    {
+        return bd.doubleValue();
+    }
+}
---
+++ src/java/org/datanucleus/store/types/converters/BigIntegerLongConverter.java
@@ -0,0 +1,50 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.math.BigInteger;
+
+import org.datanucleus.exceptions.NucleusDataStoreException;
+
+/**
+ * Class to handle the conversion between java.math.BigInteger and a Long form.
+ */
+public class BigIntegerLongConverter implements TypeConverter<BigInteger, Long>
+{
+    public BigInteger toMemberType(Long val)
+    {
+        if (val == null)
+        {
+            return null;
+        }
+
+        try
+        {
+            return BigInteger.valueOf(val);
+        }
+        catch (NumberFormatException nfe)
+        {
+            throw new NucleusDataStoreException(LOCALISER.msg("016002", val, BigInteger.class.getName()), nfe);
+        }
+    }
+
+    public Long toDatastoreType(BigInteger bi)
+    {
+        return bi.longValue();
+    }
+}
