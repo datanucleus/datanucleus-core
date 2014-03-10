c5f502189c188546a39cdda21d6afd0f7e0bff8a
andyjefferson <andy@datanucleus.org>
2014-03-10 10:36:18 +0000

    fix casting

--- src/java/org/datanucleus/store/types/simple/Properties.java
+++ src/java/org/datanucleus/store/types/simple/Properties.java
@@ -67,7 +67,7 @@ public class Properties extends java.util.Properties implements SCOMap, Cloneabl
         java.util.Map m = (java.util.Map)o;
         if (m != null)
         {
-            delegate = new java.util.Properties((Properties)m); // Make copy of container rather than using same memory
+            delegate = new java.util.Properties((java.util.Properties) m); // Make copy of container rather than using same memory
         }
         else
         {
