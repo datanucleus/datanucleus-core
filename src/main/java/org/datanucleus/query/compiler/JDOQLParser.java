edc697815caaa7251f5685259836295cd0b1716f
andyjefferson <andy@datanucleus.org>
2015-09-11 15:34:37 +0100

    [NUCCORE-1349] Handle Long and BigInteger literals depending on size of the integral value

--- src/main/java/org/datanucleus/query/compiler/JDOQLParser.java
+++ src/main/java/org/datanucleus/query/compiler/JDOQLParser.java
@@ -1157,7 +1157,16 @@ public class JDOQLParser implements Parser
         }
         else if ((iLiteral = p.parseIntegerLiteral()) != null)
         {
-            litValue = Long.valueOf(iLiteral.longValue());
+            // Represent as BigInteger or Long depending on length
+            String longStr = "" + iLiteral.longValue();
+            if (longStr.length() < iLiteral.toString().length())
+            {
+                litValue = iLiteral;
+            }
+            else
+            {
+                litValue = iLiteral.longValue();
+            }
         }
         else if ((bLiteral = p.parseBooleanLiteral()) != null)
         {
