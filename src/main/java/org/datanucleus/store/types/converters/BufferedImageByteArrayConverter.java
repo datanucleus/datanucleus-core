3b436aafb871c29e45658a2c3d3d57b3a1586f03
andyjefferson <andy@datanucleus.org>
2016-03-31 09:09:18 +0100

    Add converter for BufferedImage -> bye[]

---
+++ src/main/java/org/datanucleus/store/types/converters/BufferedImageByteArrayConverter.java
@@ -0,0 +1,80 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.datanucleus.store.types.converters.TypeConverter;
+
+/**
+ * Convenience class to handle Java serialisation of a BufferedImage object to/from byte[].
+ */
+public class BufferedImageByteArrayConverter implements TypeConverter<BufferedImage, byte[]>
+{
+
+    private static final long serialVersionUID = 585211414298721468L;
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toDatastoreType(java.lang.Object)
+     */
+    public byte[] toDatastoreType(BufferedImage memberValue)
+    {
+        if (memberValue == null)
+        {
+            return null;
+        }
+
+        byte[] bytes = null;
+        try
+        {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(8192);
+            ImageIO.write(memberValue, "jpg", baos);
+            bytes = baos.toByteArray();
+            baos.close();
+        }
+        catch (IOException e)
+        {
+        }
+        return bytes;
+    }
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toMemberType(java.lang.Object)
+     */
+    public BufferedImage toMemberType(byte[] datastoreValue)
+    {
+        if (datastoreValue == null || datastoreValue.length == 0)
+        {
+            return null;
+        }
+
+        try
+        {
+            return ImageIO.read(new ByteArrayInputStream(datastoreValue));
+        }
+        catch (IOException ioe)
+        {
+            return null;
+        }
+    }
+}
