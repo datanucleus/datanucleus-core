231d1c8952a4154a8c57a16388c46b4c1d5fcba3
andyjefferson <andy@datanucleus.org>
2016-04-08 11:31:07 +0100

    [NUCCORE-1400] Support for MONTH_JAVA in-memory

---
+++ src/main/java/org/datanucleus/query/inmemory/TemporalMonthJavaMethod.java
@@ -0,0 +1,73 @@
+/**********************************************************************
+Copyright (c) 2016 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.query.inmemory;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.datanucleus.exceptions.NucleusException;
+import org.datanucleus.exceptions.NucleusUserException;
+import org.datanucleus.query.expression.InvokeExpression;
+import org.datanucleus.util.Localiser;
+
+/**
+ * Evaluator for the method "MONTH_JAVA({dateExpr})".
+ * This returns the equivalent of the month for java.util.Date i.e (0,11)
+ */
+public class TemporalMonthJavaMethod implements InvocationEvaluator
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.query.evaluator.memory.InvocationEvaluator#evaluate(org.datanucleus.query.expression.InvokeExpression, org.datanucleus.query.evaluator.memory.InMemoryExpressionEvaluator)
+     */
+    public Object evaluate(InvokeExpression expr, Object invokedValue, InMemoryExpressionEvaluator eval)
+    {
+        if (invokedValue == null)
+        {
+            return Boolean.FALSE;
+        }
+        if (!(invokedValue instanceof Date))
+        {
+            throw new NucleusException(Localiser.msg("021011", expr.getOperation(), invokedValue.getClass().getName()));
+        }
+
+        if (invokedValue instanceof Date)
+        {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime((Date)invokedValue);
+            return Integer.valueOf(cal.get(Calendar.MONTH));
+        }
+        else if (invokedValue instanceof Calendar)
+        {
+            return Integer.valueOf(((Calendar)invokedValue).get(Calendar.MONTH));
+        }
+        else if (invokedValue instanceof LocalDate)
+        {
+            return ((LocalDate)invokedValue).getMonthValue()-1;
+        }
+        else if (invokedValue instanceof LocalDateTime)
+        {
+            return ((LocalDateTime)invokedValue).getMonthValue()-1;
+        }
+        else
+        {
+            throw new NucleusUserException("We do not currently support MONTH_JAVA() with argument of type " + invokedValue.getClass().getName());
+        }
+    }
+}
