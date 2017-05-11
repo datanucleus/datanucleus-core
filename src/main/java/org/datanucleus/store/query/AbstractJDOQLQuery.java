1c2ba0bfdd4579b9a9105859c593b3f14752a1ea
andyjefferson <andy@datanucleus.org>
2017-05-11 07:56:04 +0100

    with null check too

--- src/main/java/org/datanucleus/store/query/AbstractJDOQLQuery.java
+++ src/main/java/org/datanucleus/store/query/AbstractJDOQLQuery.java
@@ -417,7 +417,7 @@ public abstract class AbstractJDOQLQuery extends AbstractJavaQuery
     {
         if ((resultClass != null || resultClassName != null) && result == null)
         {
-            if (resultClass == Object[].class || resultClassName.equals(Object[].class.getName()))
+            if (resultClass == Object[].class || (resultClassName != null && resultClassName.equals(Object[].class.getName())))
             {
                 // User has specified the blatantly obvious, so ignore it
             }
