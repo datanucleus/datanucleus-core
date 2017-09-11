178bd71ae0627f865792efc364c8a1ba7f30546d
andyjefferson <andy@datanucleus.org>
2017-09-11 16:20:46 +0100

    Move string constructor handling to ObjectId. Fixes #263

--- src/main/java/org/datanucleus/identity/ObjectId.java
+++ src/main/java/org/datanucleus/identity/ObjectId.java
@@ -20,8 +20,14 @@ package org.datanucleus.identity;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.text.DateFormat;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.datanucleus.enhancer.EnhancementHelper;
 import org.datanucleus.exceptions.NucleusUserException;
 
 /**
@@ -58,7 +64,7 @@ public class ObjectId extends SingleFieldId
             }
             keyString = paramString.substring(indexOfDelimiter + 1);
             keyClassName = paramString.substring(0, indexOfDelimiter);
-            key = EnhancementHelper.construct(keyClassName, keyString);
+            key = construct(keyClassName, keyString);
         }
         else
         {
@@ -150,5 +156,187 @@ public class ObjectId extends SingleFieldId
     {
         super.readExternal(in);
         key = in.readObject();
+    }
+
+    /**
+     * Construct an instance of a key class using a String as input.
+     * Classes without a String constructor (such as those in java.lang and java.util) will use this interface for constructing new instances. 
+     * The result might be a singleton or use some other strategy.
+     */
+    public interface StringConstructor
+    {
+        /**
+         * Construct an instance of the class for which this instance is registered.
+         * @param s the parameter for construction
+         * @return the constructed object
+         */
+        public Object construct(String s);
+    }
+
+    /**
+     * Special StringConstructor instances for use with specific classes that have no public String constructor. 
+     * The Map is keyed on class instance and the value is an instance of StringConstructor.
+     */
+    static final Map<Class, StringConstructor> stringConstructorMap = new HashMap<Class, StringConstructor>();
+
+    /**
+     * Register special StringConstructor instances. These instances are for constructing instances from
+     * String parameters where there is no String constructor for them.
+     * @param cls the class to register a StringConstructor for
+     * @param sc the StringConstructor instance
+     * @return the previous StringConstructor registered for this class
+     */
+    public static Object registerStringConstructor(Class cls, StringConstructor sc)
+    {
+        synchronized (stringConstructorMap)
+        {
+            return stringConstructorMap.put(cls, sc);
+        }
+    }
+
+    /**
+     * Construct an instance of the parameter class, using the keyString as an argument to the constructor. 
+     * If the class has a StringConstructor instance registered, use it. 
+     * If not, try to find a constructor for the class with a single String argument. Otherwise, throw a NucleusUserException.
+     * TODO Consider moving this to ObjectId or IdentityManager
+     * @param className the name of the class
+     * @param keyString the String parameter for the constructor
+     * @return the result of construction
+     */
+    public static Object construct(String className, String keyString)
+    {
+        StringConstructor stringConstructor;
+        try
+        {
+            Class<?> keyClass = Class.forName(className);
+            synchronized (stringConstructorMap)
+            {
+                stringConstructor = stringConstructorMap.get(keyClass);
+            }
+            if (stringConstructor != null)
+            {
+                return stringConstructor.construct(keyString);
+            }
+            return keyClass.getConstructor(new Class[]{String.class}).newInstance(new Object[]{keyString});
+        }
+        catch (Exception ex)
+        {
+            // ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException
+            throw new NucleusUserException("Exception in Object identity String constructor", ex);
+        }
+    }
+
+    /** The default DateFormat instance for handling java.util.Date instances. */
+    private static DateFormat dateFormat;
+
+    /**
+     * Register the default special StringConstructor instances.
+     */
+    static
+    {
+        // TODO Add other possible types
+        registerStringConstructor(java.util.Currency.class, new StringConstructor()
+        {
+            public Object construct(String s)
+            {
+                try
+                {
+                    return java.util.Currency.getInstance(s);
+                }
+                catch (Exception ex)
+                {
+                    throw new NucleusUserException("Exception in ObjectId String constructor for Currency", ex);
+                }
+            }
+        });
+
+        registerStringConstructor(java.util.Locale.class, new StringConstructor()
+        {
+            public Object construct(String s)
+            {
+                try
+                {
+                    String lang = s;
+                    int firstUnderbar = s.indexOf('_');
+                    if (firstUnderbar == -1)
+                    {
+                        // nothing but language
+                        return new java.util.Locale(lang);
+                    }
+                    lang = s.substring(0, firstUnderbar);
+                    String country;
+                    int secondUnderbar = s.indexOf('_', firstUnderbar + 1);
+                    if (secondUnderbar == -1)
+                    {
+                        // nothing but language, country
+                        country = s.substring(firstUnderbar + 1);
+                        return new java.util.Locale(lang, country);
+                    }
+                    country = s.substring(firstUnderbar + 1, secondUnderbar);
+                    String variant = s.substring(secondUnderbar + 1);
+                    return new java.util.Locale(lang, country, variant);
+                }
+                catch (Exception ex)
+                {
+                    throw new NucleusUserException("Exception in ObjectId String constructor for Locale", ex);
+                }
+            }
+        });
+
+        /*
+         * This requires the following privileges for EnhancementHelper in the security permissions file: 
+         * permission java.util.PropertyPermission "user.country", "read"; 
+         * permission java.util.PropertyPermission "user.timezone", "read,write";
+         * permission java.util.PropertyPermission "java.home", "read"; 
+         * If these permissions are not present, or there is some other problem getting the default date format, a simple formatter is returned.
+         */
+        DateFormat result = null;
+        try
+        {
+            result = AccessController.doPrivileged(new PrivilegedAction<DateFormat>()
+            {
+                public DateFormat run()
+                {
+                    return DateFormat.getDateTimeInstance();
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            result = DateFormat.getInstance();
+        }
+        dateFormat = result;
+
+        registerStringConstructor(java.util.Date.class, new StringConstructor()
+        {
+            public synchronized Object construct(String s)
+            {
+                try
+                {
+                    // first, try the String as a Long
+                    return new java.util.Date(Long.parseLong(s));
+                }
+                catch (NumberFormatException ex)
+                {
+                    // not a Long; try the formatted date
+                    ParsePosition pp = new ParsePosition(0);
+                    java.util.Date result = dateFormat.parse(s, pp);
+                    if (result == null)
+                    {
+                        String dateFormatPattern = null;
+                        if (dateFormat instanceof SimpleDateFormat)
+                        {
+                            dateFormatPattern = ((SimpleDateFormat)dateFormat).toPattern();
+                        }
+                        else
+                        {
+                            dateFormatPattern = "Unknown message";
+                        }
+                        throw new NucleusUserException("Exception in ObjectId String constructor for Date", new Object[] {s, Integer.valueOf(pp.getErrorIndex()), dateFormatPattern});
+                    }
+                    return result;
+                }
+            }
+        });
     }
 }
