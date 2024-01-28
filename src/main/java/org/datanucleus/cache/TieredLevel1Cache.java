00f5ade2b34f62b79734fe063f20cc44fd5495fe
andyjefferson <andy@datanucleus.org>
2024-01-28 13:36:58 +0000

    copyright fixes and brace policy

--- src/main/java/org/datanucleus/cache/TieredLevel1Cache.java
+++ src/main/java/org/datanucleus/cache/TieredLevel1Cache.java
@@ -1,17 +1,20 @@
 /**********************************************************************
- Copyright (c) 2006 Andy Jefferson and others. All rights reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Copyright (c) 2024 kraendavid and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- **********************************************************************/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
 package org.datanucleus.cache;
 
 import org.datanucleus.state.DNStateManager;
@@ -22,7 +25,8 @@ import org.datanucleus.state.DNStateManager;
  * cache - Level1 caches implementing this interface could optimize
  * the filtering of finding non-hollow PC objects.
  */
-public interface TieredLevel1Cache extends Level1Cache {
+public interface TieredLevel1Cache extends Level1Cache 
+{
     /**
      * Return non-hollow state managers that could be considered as dirty during commit phase.
      * @return non-hollow state managers to consider during commit phase
